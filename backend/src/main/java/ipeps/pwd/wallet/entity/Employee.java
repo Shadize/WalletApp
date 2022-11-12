0c1946d1ec7db30220ff037bb6ed04c523683bcd
IrwinnSama <warnier.fabian@gmail.com>
2022-11-12 14:34:24 +0100

    Creation Entity
    Creation Entity

---
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Employee.java
@@ -0,0 +1,38 @@
+package ipeps.pwd.wallet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.StringBuilderFormattable;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "employee_id", updatable = false, nullable = false)
+    UUID employee_id;
+
+    String lastname;
+    String firstname;
+    Boolean active;
+    String deleted_by;
+    String address;
+    String gender;
+    Date birthday;
+    String ssin;
+    String status;
+    String deleted;
+
+}
---
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Salary.java
@@ -0,0 +1,4 @@
+package ipeps.pwd.wallet.entity;
+
+public class Salary {
+}
