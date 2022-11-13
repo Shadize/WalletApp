d3b5490d14026307305b6f6335e5f020589ce308
Laurent Klein <53668539+shadysx@users.noreply.github.com>
2022-11-13 15:27:08 +0100

    Adding entities: Company, Skill, Document

--- backend/src/main/java/ipeps/pwd/wallet/entity/Document.java
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Document.java
@@ -1,4 +1,29 @@
 package ipeps.pwd.wallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Document {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "document_id", updatable = false, nullable = false)
+    UUID document_id;
+
+    String title;
+    String path;
+    String content;
+    String type;
+    Date created_date;
 }
--- backend/src/main/java/ipeps/pwd/wallet/entity/Skill.java
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Skill.java
@@ -1,4 +1,26 @@
 package ipeps.pwd.wallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Skill {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "skill_id", updatable = false, nullable = false)
+    UUID skill_id;
+
+    String title;
+    String description;
 }
