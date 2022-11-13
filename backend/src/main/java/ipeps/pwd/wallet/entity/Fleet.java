2236e4067072e1ae9c5ee8f7d8e0eaa20befdfb0
IrwinnSama <warnier.fabian@gmail.com>
2022-11-13 15:47:53 +0100

    CreationFleetAndOrganization

--- backend/src/main/java/ipeps/pwd/wallet/entity/Fleet.java
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Fleet.java
@@ -1,4 +1,29 @@
 package ipeps.pwd.wallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Fleet {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "fleet_id", updatable = false, nullable = false)
+    UUID fleet_id;
+    String title;
+    String description;
+    String type;
+    Float cost;
+
 }
--- backend/src/main/java/ipeps/pwd/wallet/entity/Organization.java
+++ backend/src/main/java/ipeps/pwd/wallet/entity/Organization.java
@@ -1,4 +1,26 @@
 package ipeps.pwd.wallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Organization {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "organization_id", updatable = false, nullable = false)
+    UUID organization_id;
+
+    String name;
+    String description;
 }
