659856ffb9f6a9cec0e0d88532de3ef2eeef99ac
IrwinnSama <warnier.fabian@gmail.com>
2022-11-21 18:54:48 +0100

    Implémentation service Employee, fleet et skill

--- backend/src/main/java/ipeps/pwd/wallet/service/impl/EmployeeServiceImpl.java
+++ backend/src/main/java/ipeps/pwd/wallet/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,9 @@ package ipeps.pwd.wallet.service.impl;
 
 import ipeps.pwd.wallet.entity.Company;
 import ipeps.pwd.wallet.entity.Employee;
+import ipeps.pwd.wallet.entity.Salary;
+import ipeps.pwd.wallet.entity.builder.EmployeeBuilder;
+import ipeps.pwd.wallet.entity.builder.SalaryBuilder;
 import ipeps.pwd.wallet.entity.payload.EmployeeCreatePayload;
 import ipeps.pwd.wallet.entity.payload.EmployeeUpdatePayload;
 import ipeps.pwd.wallet.repository.EmployeeRepository;
@@ -22,26 +25,75 @@ public class EmployeeServiceImpl  implements EmployeeService {
 
     @Override
     public List<Employee> list() {
-        return null;
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee detail(UUID employeeId) {
-        return null;
+        return employeeRepository.findById(employeeId).orElse(null);
+    }
+    @Override
+    public Employee create(EmployeeCreatePayload payload)
+    {
+        try
+        {
+            Employee employee = new EmployeeBuilder()
+                    .setLastname(payload.getLastname())
+                    .setFirstname(payload.getFirstname())
+                    .setActive(payload.getActive())
+                    .setDeleted_by(payload.getDeleted_by())
+                    .setAddress(payload.getAddress())
+                    .setGender(payload.getGender())
+                    .setBirthday(payload.getBirthday())
+                    .setSsin(payload.getSsin())
+                    .setStatus(payload.getStatus())
+                    .setDeleted(payload.getDeleted())
+                    .build();
+            return this.employeeRepository.save(employee);
+
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
     }
 
     @Override
-    public Employee create(EmployeeCreatePayload payload) {
-        return null;
+    public Employee update(EmployeeUpdatePayload payload)
+    {
+        Employee detail = this.detail(payload.getEmployee_id());
+        if(detail != null)
+        {
+            detail.setLastname(payload.getLastname());
+            detail.setFirstname(payload.getFirstname());
+            detail.setActive(payload.getActive());
+            detail.setDeleted_by(payload.getDeleted_by());
+            detail.setAddress(payload.getAddress());
+            detail.setGender(payload.getGender());
+            detail.setBirthday(payload.getBirthday());
+            detail.setSsin(payload.getSsin());
+            detail.setStatus(payload.getStatus());
+            detail.setDeleted(payload.getDeleted());
+            return this.employeeRepository.save(detail);
+        }
+        return detail;
     }
 
     @Override
-    public Employee update(EmployeeUpdatePayload payload) {
-        return null;
-    }
-
-    @Override
-    public boolean delete(UUID employeeId) {
-        return false;
+    public boolean delete(UUID employeeId)
+    {
+        try
+        {
+            Employee detail = this.detail(employeeId);
+            if (detail != null)
+            {
+                this.employeeRepository.delete(detail);
+            }
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
     }
 }
--- backend/src/main/java/ipeps/pwd/wallet/service/impl/SkillServiceImpl.java
+++ backend/src/main/java/ipeps/pwd/wallet/service/impl/SkillServiceImpl.java
@@ -1,6 +1,8 @@
 package ipeps.pwd.wallet.service.impl;
 
+import ipeps.pwd.wallet.entity.Salary;
 import ipeps.pwd.wallet.entity.Skill;
+import ipeps.pwd.wallet.entity.builder.SkillBuilder;
 import ipeps.pwd.wallet.entity.payload.SkillCreatePayload;
 import ipeps.pwd.wallet.entity.payload.SkillUpdatePayload;
 import ipeps.pwd.wallet.repository.SkillRepository;
@@ -19,26 +21,57 @@ public class SkillServiceImpl implements SkillService {
 
     @Override
     public List<Skill> list() {
-        return null;
+        return this.skillRepository.findAll();
     }
 
     @Override
-    public Skill detail(UUID skillId) {
-        return null;
+    public Skill detail(UUID skillId)
+    {
+        return this.skillRepository.findById(skillId).orElse(null);
     }
 
     @Override
-    public Skill create(SkillCreatePayload payload) {
-        return null;
+    public Skill create(SkillCreatePayload payload)
+    {
+        try
+        {
+            Skill skill = new SkillBuilder()
+                    .setTitle(payload.getTitle())
+                    .setDescription(payload.getDescription())
+                    .build();
+            return this.skillRepository.save(skill);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
     }
 
     @Override
-    public Skill update(SkillUpdatePayload payload) {
-        return null;
+    public Skill update(SkillUpdatePayload payload)
+    {
+        Skill detail = this.detail(payload.getSkill_id());
+        if (detail != null)
+        {
+            detail.setDescription(payload.getDescription());
+            detail.setTitle(payload.getTitle());
+            return this.skillRepository.save(detail);
+        }
+        return detail;
     }
 
     @Override
-    public boolean delete(UUID skillId) {
-        return false;
+    public boolean delete(UUID skillId)
+    {
+        try {
+            Skill detail = this.detail(skillId);
+            if (detail != null) {
+                this.skillRepository.delete(detail);
+            }
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
     }
 }
