c445d8d33f331635a4ca447497eef8a73b477c09
IrwinnSama <warnier.fabian@gmail.com>
2022-11-21 19:10:33 +0100

    update skillcontroller test mpostman

--- backend/src/main/java/ipeps/pwd/wallet/controller/SkillController.java
+++ backend/src/main/java/ipeps/pwd/wallet/controller/SkillController.java
@@ -1,7 +1,12 @@
 package ipeps.pwd.wallet.controller;
 
+import ipeps.pwd.wallet.common.entity.response.ApiCode;
+import ipeps.pwd.wallet.common.entity.response.ApiResponse;
+import ipeps.pwd.wallet.entity.payload.SkillCreatePayload;
 import ipeps.pwd.wallet.service.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SkillController {
     @Autowired
     SkillService skillService;
+
+    @PostMapping("create")
+    public ApiResponse create(@RequestBody() SkillCreatePayload payload)
+    {
+        try {
+            return new ApiResponse(true, this.skillService.create(payload), ApiCode.SKILL_CREATE_SUCCESS);
+        } catch (Exception e) {
+            return new ApiResponse(false, e.getMessage(), ApiCode.SKILL_CREATE_ERROR);
+        }
+    }
+
 
 }
