dc9bddc2acdd8f2e298fe02a33c05149bf20fb5b
Laurent Klein <53668539+shadysx@users.noreply.github.com>
2022-12-12 01:00:20 +0100

    fix fabian component

--- WalletApp/src/app/app-routing.module.ts
+++ WalletApp/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PublicGuard, SecurityGuard} from '@security/guard';
-import {FabianCompComponent} from "./modules/fabian-comp/fabian-comp.component";
 import {TestsComponent} from "./modules/tests/tests.component";
 
 const routes: Routes = [
@@ -14,10 +13,6 @@ const routes: Routes = [
     path: 'account',
     canActivate: [PublicGuard],
     loadChildren: () => import('./security/security.module').then(m => m.SecurityModule)
-  },
-  {
-    path: 'test',
-    component: FabianCompComponent
   },
   {
     path: 'test2',
