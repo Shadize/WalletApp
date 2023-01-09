3b1c174518e13c1e68d19197c3525cbac9c7e0dc
Warnier Fabian <84586807+IrwinnSama@users.noreply.github.com>
2023-01-09 22:57:26 +0100

    Fix employee backend

--- backend/src/main/java/ipeps/pwd/wallet/module/employee/entity/Employee.java
+++ backend/src/main/java/ipeps/pwd/wallet/module/employee/entity/Employee.java
@@ -1,15 +1,23 @@
 package ipeps.pwd.wallet.module.employee.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.*;
 import ipeps.pwd.wallet.module.company.entity.Company;
+import ipeps.pwd.wallet.module.contract.entity.Contract;
+import ipeps.pwd.wallet.module.document.entity.Document;
+import ipeps.pwd.wallet.module.fleet.entity.Fleet;
+import ipeps.pwd.wallet.module.salary.entity.Salary;
 import ipeps.pwd.wallet.module.skill.entity.Skill;
+import ipeps.pwd.wallet.module.skill.service.SkillService;
+import ipeps.pwd.wallet.module.timesheet.entity.Timesheet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +26,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "employeeId")
 public class Employee {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -36,11 +45,33 @@ public class Employee {
     String status;
     Boolean deleted;
 
+    @JsonIgnoreProperties({"employees"})
     @ManyToOne()
     @JoinColumn(name = "company_FK",referencedColumnName = "company_id", nullable = false, foreignKey=@ForeignKey(name = "employee_company_fk"))
     private Company company;
 
-    @JsonIgnoreProperties("employees")
+    @JsonIgnoreProperties({"employee"})
+    @OneToMany(mappedBy = "employee")
+    List<Timesheet> timesheets;
+
+    @JsonIgnoreProperties({"employee"})
+    @OneToMany(mappedBy = "employee")
+    List<Document> documents;
+
+    @JsonIgnoreProperties({"employee"})
+    @OneToMany(mappedBy = "employee")
+    List<Contract> contracts;
+
+    @JsonIgnoreProperties({"employee"})
+    @OneToMany(mappedBy = "employee")
+    List<Fleet> fleets;
+
+    @JsonIgnoreProperties({"employee"})
+    @OneToMany(mappedBy = "employee")
+    List<Salary> salaries;
+
+    //@JsonIgnoreProperties({"employess","title","description"})
+    @JsonIgnoreProperties({"employees"})
     @ManyToMany()
     @JoinTable(
             name = "Employee_Skill",
@@ -49,8 +80,10 @@ public class Employee {
     List<Skill> skills;
 
     public Employee(String lastname, String firstname, Boolean active, String deletedBy,
-                    String address, String gender, Date birthday, String ssin,String status,
-                    Boolean deleted, Company company, List<Skill> skills)
+                    String address, String gender, Date birthday, String ssin, String status,
+                    Boolean deleted,
+                    List<Contract> contracts, Company company, List<Document> documents,
+                    List<Fleet> fleets, List<Salary> salaries, List<Skill> skills, List<Timesheet> timesheets)
     {
         this.lastname = lastname;
         this.firstname = firstname;
@@ -64,5 +97,10 @@ public class Employee {
         this.deleted = deleted;
         this.company = company;
         this.skills = skills;
+        this.contracts = contracts;
+        this.documents = documents;
+        this.fleets = fleets;
+        this.salaries = salaries;
+        this.timesheets = timesheets;
     }
 }
