10b09eef97f15e7d9219f1db585d7030e1650a8e
Shadize <90429515+Shadize@users.noreply.github.com>
2023-02-05 01:36:20 +0100

    backend update

--- backend/src/main/java/ipeps/pwd/wallet/security/config/RessourceServerConfiguration.java
+++ backend/src/main/java/ipeps/pwd/wallet/security/config/RessourceServerConfiguration.java
@@ -11,12 +11,26 @@ public class RessourceServerConfiguration extends ResourceServerConfigurerAdapte
     public void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
 
-        http.authorizeRequests().antMatchers("/login","/oauth/token", "/account/signup", "/account/signin","/account/refresh", "/salary/list","/salary/create", "/salary/update", "/salary/detail/{id}", "/salary/delete/{id}",
-                "/employee/list","/employee/create", "/employee/update", "/employee/detail/{id}", "/employee/delete/{id}", "/skill/create", "/skill/list", "/skill/update", "/company/create","/company/list",
+        http.authorizeRequests().antMatchers(
+                //Account
+                "/login","/oauth/token", "/account/signup", "/account/signin","/account/refresh",
+                // Salary
+                "/salary/list","/salary/create", "/salary/update", "/salary/detail/{id}", "/salary/delete/{id}",
+                // Employee
+                "/employee/list","/employee/create", "/employee/update", "/employee/detail/{id}", "/employee/delete/{id}",
+                // Skill
+                "/skill/create", "/skill/list", "/skill/update", "/skill/detail/{id}", "/skill/delete/{id}",
+                // Company
+                "/company/create","/company/list","/company/update","/company/detail/{id}", "/company/delete/{id}",
+                // Organization
                 "/organization/list","/organization/create", "/organization/update", "/organization/detail/{id}", "/organization/delete/{id}",
+                // Document
                 "/document/list","/document/create", "/document/update", "/document/detail/{id}", "/document/delete/{id}",
+                // Timesheet
                 "/timesheet/list","/timesheet/create", "/timesheet/update", "/timesheet/detail/{id}", "/timesheet/delete/{id}",
+                // Fleet
                 "/fleet/list","/fleet/create", "/fleet/update", "/fleet/detail/{id}", "/fleet/delete/{id}",
+                // Contract
                 "/contract/list","/contract/create", "/contract/update", "/contract/detail/{id}", "/contract/delete/{id}").permitAll();
 
         http.authorizeRequests().anyRequest().authenticated();
